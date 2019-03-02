596967d7911e5154f0526db9e030053e8e80997c
NyxTheShield <felipe.montero.13@sansano.usm.cl>
2019-03-01 21:22:32 -0500

    Add files via upload

--- ssbu-mod-installer.py
+++ ssbu-mod-installer.py
@@ -10,6 +10,26 @@ import sys
 import os
 import time
 
+
+def colorToFloat(t):
+    nt = ()
+    for v in t:
+        nt += ((1/255) * v, )
+    return nt
+
+# (r, g, b)
+FOLDER_COLOR = colorToFloat((230, 126, 34))
+PYFILE_COLOR = colorToFloat((71, 123, 209))
+FILE_COLOR = colorToFloat((41, 128, 185))
+TILED_DOUBLE = 1
+state = "file_manager"
+debugger = []
+sys.argv = [""]  # workaround needed for runpy
+
+def logger(text):
+    with open("LOG.txt", "a") as myfile:
+        myfile.write(text+'\n')
+
 def _isValidHex(s):
     try:
         int(s, 16)
@@ -24,43 +44,38 @@ def getFileBaseName(path):
 def installMod(archive, modFilename):
     offset = int(getFileBaseName(modFilename), 16)
     archive.seek(offset)
+    logger("Installing Mod:"+modFilename)
     with open(modFilename, 'rb') as f:
+        logger("Before Read...")
         modContents = f.read()
+        logger("Before ModSize...")
         modSize = len(modContents)
         backup = archive.read(modSize)
+        logger("Making Backup...")
         if not os.path.exists(modFilename + '.backup'):
             with open(modFilename + '.backup', 'wb') as f_backup:
                 f_backup.write(backup)
         archive.seek(offset)
         archive.write(modContents)
     print("Mod '{}' successfully installed".format(modFilename))
+    logger("Mod '{}' successfully installed".format(modFilename))
 
 def uninstallMod(archive, modFilename):
     offset = int(getFileBaseName(modFilename), 16)
     archive.seek(offset)
+    logger("Uninstalling Mod:"+modFilename)
     with open(modFilename, 'rb') as f:
+        logger("Before Read...")
         modContents = f.read()
+        logger("Before ModSize...")
         modSize = len(modContents)
         backup = archive.read(modSize)
         archive.seek(offset)
+        logger("Before Write...")
         archive.write(modContents)
     os.remove(modFilename)
     print("Mod '{}' successfully uninstalled".format(modFilename))
-
-sys.argv = [""]  # workaround needed for runpy
-
-def colorToFloat(t):
-    nt = ()
-    for v in t:
-        nt += ((1/255) * v, )
-    return nt
-
-# (r, g, b)
-FOLDER_COLOR = colorToFloat((230, 126, 34))
-PYFILE_COLOR = colorToFloat((71, 123, 209))
-FILE_COLOR = colorToFloat((41, 128, 185))
-TILED_DOUBLE = 1
-state = "file_manager"
+    logger("Mod '{}' successfully uninstalled".format(modFilename))
 
 
 def run_python_module(mod: str, mode):
@@ -71,15 +86,18 @@ def run_python_module(mod: str, mode):
     clear_terminal()
     if not os.path.exists("data.arc"):
         print("Make sure you're running the script beside your data.arc")
+        logger("Make sure you're running the script beside your data.arc")
         renderer.shutdown()
     else:
         with open('data.arc', 'r+b') as archive:
             if not os.path.exists(mod):
                 print("'{}' not valid mod path".format(mod))
+                logger("'{}' not valid mod path".format(mod))
                 renderer.shutdown()
             fileName = getFileBaseName(mod)
             if not _isValidHex(fileName):
                 print("Filename '{}' not valid hex number".format(fileName))
+                logger("'{}' not valid mod path".format(mod))
                 renderer.shutdown()
 
             if mode == 1:
@@ -89,12 +107,50 @@ def run_python_module(mod: str, mode):
             global state
             state = "installed"
     imguihelper.initialize()
+
+def batch_install(folder):
+    global state
+    global debugger
+    mode = 1
+    ending =""
+    path = folder+"/"
+
+
+    files = os.listdir(folder)
+    for file in files:
+        if ".backup" in file:
+            mode = 2
+            break
+    if mode == 2:
+        logger("Modo Detectado: Desinstalar")
+        debugger.append("Modo Detectado: Desinstalar")
+        ending =".backup"
+    else:
+        logger("Modo Detectado: Instalar")
+        debugger.append("Modo Detectado: Instalar")
+        ending =""
     
+    mod_files = open(path+"Install.txt", 'r')
+    for file in mod_files:
+        file = file.strip()
+        debugger.append(path+file+ending)
+        logger(path+file+ending)
+        run_python_module(path+file+ending, mode)
+    debugger.append(str(mode))
+    mod_files.close()
+
+    if mode == 1:
+        state = "batch_i"
+    if mode == 2:
+        state = "batch_u"
 
 def main():
     global state
+    global debugger
+    current_batch_mod = ""
     ctime = 0
     renderer = NXRenderer()
+    
     if os.path.isdir("sdmc:/ReiNX"):
         currentDir = "sdmc:/ReiNX/titles/01006A800016E000/romfs"
         os.chdir(currentDir)
@@ -134,7 +190,7 @@ def main():
                 if os.path.isdir(e):
                     dirs.append(e)
                 else:
-                    if not e.endswith(".exe") and not e.endswith(".bat") and not e.endswith(".backup") and not e.endswith(".arc"):
+                    if not e.endswith(".exe") and not e.endswith(".bat") and not e.endswith(".backup") and not e.endswith(".arc") and not e.endswith(".txt"):
                         files.append(e)
                     if e.endswith(".backup"):
                         backups.append(e)
@@ -142,7 +198,28 @@ def main():
             dirs = sorted(dirs)
             files = sorted(files)
             backups = sorted(backups)
-                
+
+            for e in dirs:
+                flag = False
+                added_text = ""
+                batch_files = os.listdir(e)
+                for file in batch_files:
+                    if ".backup" in file:
+                        imgui.push_style_color(imgui.COLOR_BUTTON, *FOLDER_COLOR)
+                        flag = True
+                        added_text = "Uninstall "
+                        break
+                if flag == False:
+                    imgui.push_style_color(imgui.COLOR_BUTTON, *PYFILE_COLOR)
+                    added_text = "Install "
+                    
+                if imgui.button(added_text + e + "/", width=200, height=60):
+                    ctime = time.time()
+                    current_batch_mod = e
+                    batch_install(e)
+                imgui.pop_style_color(1)
+ 
+            
             for e in files:
                 if not e.endswith(".exe") and not e.endswith(".bat") and not e.endswith(".backup") and not e.endswith(".arc"):
                     imgui.push_style_color(imgui.COLOR_BUTTON, *PYFILE_COLOR)
@@ -190,6 +267,50 @@ def main():
             imgui.end()
             imgui.render()
             renderer.render()
+            
+        while state == "batch_i":
+            
+            imgui.new_frame()
+            
+            width, height = renderer.io.display_size
+            imgui.set_next_window_size(width, height)
+            imgui.set_next_window_position(0, 0)
+            imgui.begin("", 
+                flags=imgui.WINDOW_NO_TITLE_BAR | imgui.WINDOW_NO_RESIZE | imgui.WINDOW_NO_MOVE | imgui.WINDOW_NO_SAVED_SETTINGS
+            )
+            
+            if time.time()- ctime > 5:
+                state = "file_manager"
+                ctime = 0
+                
+            imgui.text("Batch Mod Sucesfully Installed")
+
+            imgui.end()
+            imgui.render()
+            renderer.render()
+
+        while state == "batch_u":
+            
+            imgui.new_frame()
+            
+            width, height = renderer.io.display_size
+            imgui.set_next_window_size(width, height)
+            imgui.set_next_window_position(0, 0)
+            imgui.begin("", 
+                flags=imgui.WINDOW_NO_TITLE_BAR | imgui.WINDOW_NO_RESIZE | imgui.WINDOW_NO_MOVE | imgui.WINDOW_NO_SAVED_SETTINGS
+            )
+            
+            if time.time()- ctime > 5:
+                state = "file_manager"
+                ctime = 0
+                
+            imgui.text("Batch Mod Uninstalled")
+
+
+            imgui.end()
+            imgui.render()
+            renderer.render()
+    
     
 
 
